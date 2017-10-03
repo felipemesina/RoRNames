45ab482e3836cd67d0f12802ac3a56044ac86a75
Felipe <felipejmesina@gmail.com>
2017-10-03 14:38:55 -0700

    complete

---
+++ names.rb
@@ -0,0 +1,15 @@
+
+
+a = {first_name: "Michael", last_name: "Choi"}
+b = {first_name: "John", last_name: "Doe"}
+c = {first_name: "Jane", last_name: "Doe"}
+d = {first_name: "James", last_name: "Smith"}
+e = {first_name: "Jennifer", last_name: "Smith"}
+names = [a, b, c, d, e]
+puts "You have " + "#{names.length}" + " names in the 'names' array."
+
+names.each do |name|
+  puts "The name is #{name[:first_name]} #{name[:last_name]}"
+end
+
+#puts x.each_index {|index| x[index] = x.delete(index) if x[index].length < 5}
